a68ae8bc7a4dd02b2bcf801dc1b4180910b56614
RahmanCodeCraft <Abdulrahmanmern@gmail.com>
2024-07-26 22:45:55 +0500

    Implement Pagination for the table for better UX

--- src/Content.js
+++ src/Content.js
@@ -16,10 +16,13 @@ import {
   fetchLikedFormSubmissions,
 } from "./service/mockServer";
 import Popup from "./Popup";
+import TablePagination from "@mui/material/TablePagination";
 
 export default function Content() {
   const [notification, setNotification] = useState([]);
   const [likedSubmissions, setLikedSubmissions] = useState([]);
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
 
   useEffect(() => {
     fetchLikedSubmissions();
@@ -58,6 +61,14 @@ export default function Content() {
       prevNotifications.filter((toast) => toast.id !== id)
     );
   };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   return (
     <Box sx={{ marginTop: 3 }}>
@@ -74,7 +85,7 @@ export default function Content() {
             />
           );
         })}
-        <div>
+        <div style={{ marginTop: "20px" }}>
           <TableContainer component={Paper}>
             <Table>
               <TableHead>
@@ -103,29 +114,39 @@ export default function Content() {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {likedSubmissions.map((submission, index) => {
-                  const { firstName, lastName, email } = submission.data;
-                  return (
-                    <TableRow
-                      key={submission.id}
-                      sx={{
-                        backgroundColor:
-                          index % 2 === 0 ? "#f5f5f5" : "#ffffff",
-                        "&:hover": {
-                          backgroundColor: "#e0f7fa",
-                          cursor: "pointer",
-                        },
-                      }}
-                    >
-                      <TableCell>{firstName}</TableCell>
-                      <TableCell>{lastName}</TableCell>
-                      <TableCell>{email}</TableCell>
-                    </TableRow>
-                  );
-                })}
+                {likedSubmissions
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((submission, index) => {
+                    const { firstName, lastName, email } = submission.data;
+                    return (
+                      <TableRow
+                        key={submission.id}
+                        sx={{
+                          backgroundColor:
+                            index % 2 === 0 ? "#f5f5f5" : "#ffffff",
+                          "&:hover": {
+                            backgroundColor: "#e0f7fa",
+                            cursor: "pointer",
+                          },
+                        }}
+                      >
+                        <TableCell>{firstName}</TableCell>
+                        <TableCell>{lastName}</TableCell>
+                        <TableCell>{email}</TableCell>
+                      </TableRow>
+                    );
+                  })}
               </TableBody>
             </Table>
           </TableContainer>
+          <TablePagination
+            component="div"
+            count={likedSubmissions.length}
+            page={page}
+            onPageChange={handleChangePage}
+            rowsPerPage={rowsPerPage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
         </div>
       </div>
     </Box>
